927777dc2786100ddbd9de07be831615be7e2bf0
aleksej <monsarda@yandex.ru>
2019-05-22 16:21:40 +0300

    realize classes

--- src/it/intera/homework1/Main.java
+++ src/it/intera/homework1/Main.java
@@ -2,6 +2,17 @@ package it.intera.homework1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main class running");
+        Cat cat = new Cat();
+        Human human = new Human();
+        Robot robot = new Robot();
+
+        cat.jump();
+        cat.run();
+
+        human.jump();
+        human.run();
+
+        robot.jump();
+        robot.run();
     }
 }
--- src/it/intera/homework1/Robot.java
+++ src/it/intera/homework1/Robot.java
@@ -3,11 +3,11 @@ package it.intera.homework1;
 public class Robot implements Ability{
     @Override
     public void run() {
-
+        System.out.println(getClass().getSimpleName() + " бежит");
     }
 
     @Override
     public void jump() {
-
+        System.out.println(getClass().getSimpleName() + " прыгает");
     }
 }
